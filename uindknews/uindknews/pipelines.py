f665124741f105fe3525ed03d58f5e32df472601
Mohammad Husni Mubaraq <mohhusnimubaraq@gmail.com>
2025-10-20 13:44:26 +0800

    [FIX] cleaning data

--- uindknews/uindknews/pipelines.py
+++ uindknews/uindknews/pipelines.py
@@ -6,8 +6,41 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from datetime import datetime
+import locale
 
 
 class UindknewsPipeline:
     def process_item(self, item, spider):
+        return item
+
+class CleaningdataPipeline:
+    def process_item(self, item, spider):
+        try:
+            locale.setlocale(locale.LC_ALL, 'id_ID')
+        except locale.Error:
+            print('Locale Indonesia Not Support')
+            
+        author = item.get('author')
+        if author:
+            item['author'] = author.strip()
+        
+        else:
+            item['author'] = "Anonymous"
+            
+        issued = item.get('issued')
+        if issued:
+            try:
+                str_to_date = datetime.strptime(issued,'%B %d, %Y')
+                item['issued'] = str_to_date.strftime('%d/%m/%Y')
+            
+            except ValueError:
+                item['issued'] = "Date Not Found"
+                    
+        else:
+            item['issued'] = "Date Not Found"
+            
+        news_content = item.get('news_content')
+        news_content = news_content.replace('/n',' ').replace('/r',' ')
+        item['news_content'] = news_content.strip()
         return item
--- uindknews/uindknews/settings.py
+++ uindknews/uindknews/settings.py
@@ -58,9 +58,10 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "uindknews.pipelines.UindknewsPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "uindknews.pipelines.UindknewsPipeline": 300,
+   "uindknews.pipelines.CleaningdataPipeline": 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
